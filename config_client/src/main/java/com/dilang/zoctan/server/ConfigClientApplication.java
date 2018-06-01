be58eb71b939892bcbc658d2017380d14c9ac9c5
zoctan <752481828@qq.com>
2018-06-01 11:27:04 +0800

    update

--- config_client/src/main/java/com/dilang/zoctan/server/ConfigClientApplication.java
+++ config_client/src/main/java/com/dilang/zoctan/server/ConfigClientApplication.java
@@ -2,13 +2,11 @@ package com.dilang.zoctan.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
-public class ConfigApplication {
+public class ConfigClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class, args);
+        SpringApplication.run(ConfigClientApplication.class, args);
     }
 }
