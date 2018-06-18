8c434fb7e4d69b39ab5695ae75cffd5e7464507a
Zoctan <752481828@qq.com>
2018-06-18 14:02:13 +0800

    update

--- configServer/src/main/java/com/zoctan/ConfigServerApplication.java
+++ configServer/src/main/java/com/zoctan/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.zoctan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.bootstrap.encrypt.RsaProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Zoctan
